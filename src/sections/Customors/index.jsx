344d59670fa986057a19287595bad26648a98926
Yonathan Levi <jonilevi@icloud.com>
2024-05-15 22:00:53 -0400

    delete all mager use client

--- src/sections/Customors/index.jsx
+++ src/sections/Customors/index.jsx
@@ -143,7 +143,7 @@ export default function Customors() {
       />
       <h2 className="mx-3 xs:mx-8 lg:mx-32 -mt-10">לקוחות מרוצים שלנו</h2>
 
-      <Reviews />
+      {/* <Reviews /> */}
       <ul className="overflow-hidden relative pb-6 inset-x-0 pt-12  before:md:h-36 before:lg:h-44 before:w-3/12 before:absolute before:bg-gradient-to-l before:from-theme-dark before:to-transparent before:bottom-0  before:top-0 after:absolute after:bg-gradient-to-r after:from-theme-dark after:to-transparent after:w-3/12 before:md:from-35% after:md:from-35% after:left-0 after:top-0 after:bottom-0  before:z-[1] after:z-[1] after:md:h-36 after:lg:h-44">
         <div
           dir="ltr"
