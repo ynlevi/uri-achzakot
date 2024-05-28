3af6d956ce825dd2b3e2a3cfa70091e2e6875661
Yonathan Levi <jonilevi@icloud.com>
2024-05-28 14:58:51 -0400

    finishing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Assistant } from "next/font/google";
+import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -27,13 +28,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="NDEqAdJBeTlAQ-sbazUVUD7_NAlZmzQUZwNa2tVkRHY"
-        />
-      </head>
-
       <body className={assistant.className}>
         <Header />
         <Main>{children}</Main>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 //sections:
+import React from "react";
 import Hero from "@/sections/Hero";
 import Service from "@/sections/Service";
 import Uniqueness from "@/sections/Uniqueness/index";
 
 import Customors from "@/sections/Customors";
+import Head from "next/head";
 
 export default function Home() {
   return (
